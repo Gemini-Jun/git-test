618ee3cefd1333a3fef85c2f8e91a25240c2e0bc
alpha <siusiulok@outlook.com>
2021-07-10 23:05:36 +0800

    second commit

--- src/main/java/com/ty/git/GitTest.java
+++ src/main/java/com/ty/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ty.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello test");
+        System.out.println("hello test2");
     }
 }
